a16c6b8ca4293c434bfde3e3e2b8288db515eed0
Svyatoslav Kochergin <SKochergin@alfabank.ru>
2023-05-04 10:52:39 +0300

    добавление soft deleted

--- src/main/java/com/SkillBox/users/controller/UsersController.java
+++ src/main/java/com/SkillBox/users/controller/UsersController.java
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -35,6 +38,11 @@ public class UsersController {
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
         return usersService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> findAllUsers(@RequestParam("isDeleted") Boolean isDeleted) {
+        return usersService.findAllUsers(isDeleted);
     }
 
     @PutMapping("/{id}")
--- src/main/java/com/SkillBox/users/service/UsersService.java
+++ src/main/java/com/SkillBox/users/service/UsersService.java
@@ -4,10 +4,17 @@ import com.SkillBox.users.entity.User;
 import com.SkillBox.users.repository.GenderRepository;
 import com.SkillBox.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Filter;
+import org.hibernate.Session;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +22,7 @@ public class UsersService {
 
     private final UserRepository userRepository;
     private final GenderRepository genderRepository;
+    private final EntityManager entityManager;
 
     @Transactional
     public User saveUser(User user) {
@@ -43,5 +51,16 @@ public class UsersService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         return saveUser(user);
+    }
+
+    @Transactional
+    public List<User> findAllUsers(boolean isDeleted){
+        Session session = entityManager.unwrap(Session.class);
+        Filter filter = session.enableFilter("deletedUserFilter");
+        filter.setParameter("isDeleted", isDeleted);
+        Iterable<User> users =  userRepository.findAll();
+        session.disableFilter("deletedUserFilter");
+        return StreamSupport.stream(users.spliterator(), false)
+                .collect(Collectors.toList());
     }
 }
